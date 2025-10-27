1d2ded7747e773b1d43b60a404abadb4d44a9c44
ftessiKeytos <francescojtessi@icloud.com>
2025-10-27 11:26:17 +0100

    fixing dynamic page

--- src/app/api/ticket-types/route.ts
+++ src/app/api/ticket-types/route.ts
@@ -3,6 +3,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
 
+export const dynamic = 'force-dynamic';
+
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
